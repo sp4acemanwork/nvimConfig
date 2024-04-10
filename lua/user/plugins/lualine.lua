3ae57c21e9e24f1a9e5fe952247ab3b0afae9bd8
James Shufelt <jshufelt@uark.edu>
2024-04-10 16:54:36 -0500

    lualine theme

--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -7,7 +7,7 @@ return
     {
       options = {
         icons_enabled = true,
-        theme = 'auto',
+        theme = 'material',
         component_separators = { left = '', right = ''},
         section_separators = { left = '', right = ''},
         disabled_filetypes = {
