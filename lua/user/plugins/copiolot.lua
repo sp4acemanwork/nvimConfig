85c7a6a477f49304ee8c229d0d17b3f07e8a2409
James Shufelt <jamesshufeltiii@gmail.com>
2025-07-14 23:32:11 -0500

    forgot changes

---
+++ lua/user/plugins/copiolot.lua
@@ -0,0 +1,12 @@
+return {
+  "github/copilot.vim",
+  config = function ()
+    local status_ok, copilot = pcall(require('gitsigns').setup)
+
+    if not status_ok then
+      vim.error("Error something happened with copilot")
+    end
+    
+  end
+}
+
