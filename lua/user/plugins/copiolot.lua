0f336d845b1d89f745950694e9bb1998ba9b88e1
James Shufelt <jamesshufeltiii@gmail.com>
2025-08-18 16:34:01 -0500

    adding scala plugin

--- lua/user/plugins/copiolot.lua
+++ lua/user/plugins/copiolot.lua
@@ -1,13 +1,11 @@
-return {
---[[  "github/copilot.vim",
+--[[return {
+  "github/copilot.vim",
   config = function ()
     local status_ok, copilot = pcall(require('gitsigns').setup)
 
     if not status_ok then
       vim.error("Error something happened with copilot")
     end
-    
-  end copilot kinda sucks
-  ]]
+  end 
 }
-
+]]
