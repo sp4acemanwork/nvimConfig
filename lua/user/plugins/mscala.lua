73eb7efa7a44bf6af25949a41feec6291fcfd1e3
James Shufelt <jshufelt@uark.edu>
2025-10-14 15:24:53 -0500

    changes to scala/java env

--- lua/user/plugins/mscala.lua
+++ lua/user/plugins/mscala.lua
@@ -1,11 +1,28 @@
+-- lua/user/plugins/mscala.lua
+
 return {
   "scalameta/nvim-metals",
-  ft = { "scala", "sbt"},
+  -- Add plenary as a required dependency for nvim-metals
+  dependencies = { "nvim-lua/plenary.nvim" },
+  ft = { "scala", "sbt" }, -- Keep this limited to scala and sbt
   opts = function()
     local metals_config = require("metals").bare_config()
+
+    -- This is the most important change.
+    -- We are telling Metals to use a specific, compatible Java version
+    -- instead of letting it find your system's Java 21.
+    -- Make sure the path to your Java 17 or 11 home is correct.
+    -- You can find paths with: /usr/libexec/java_home -V
+    metals_config.server_settings = {
+      ["java-home"] = "/opt/homebrew/opt/openjdk@11", -- Or your path to JDK 11
+    }
+
     metals_config.on_attach = function(client, bufnr)
-      -- your on_attach function
+      -- Your on_attach keymaps can go here if you want them specific to Metals
+      -- For example:
+      -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
     end
+
     return metals_config
   end,
   config = function(self, metals_config)
@@ -17,5 +34,5 @@ return {
       end,
       group = nvim_metals_group,
     })
-  end
+  end,
 }
