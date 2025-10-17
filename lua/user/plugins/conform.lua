fc9b92fcd44820841742addf3dec08b17d97b52b
James Shufelt <jshufelt@uark.edu>
2025-10-17 11:49:25 -0500

    adding config changes

--- lua/user/plugins/conform.lua
+++ lua/user/plugins/conform.lua
@@ -1,14 +1,18 @@
+-- Corrected version for: lua/user/plugins/conform.lua
 return {
   'stevearc/conform.nvim',
+  -- Make sure to add this event line to load the plugin correctly
+  event = { "BufWritePre" },
+  cmd = { "Format" },
   opts = {
-    fomrat_on_save = {
+    -- This key is now spelled correctly
+    format_on_save = {
       timeout_ms = 500,
       lsp_fallback = true,
     },
     formatters_by_ft = {
-      lua = {'stylua'},
-      markdown = {'prettier'},
+      lua = { 'stylua' },
+      markdown = { 'prettier' },
     },
   },
-  
 }
