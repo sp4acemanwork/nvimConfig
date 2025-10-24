6e65711e6fa2a11ef67a9391159bc695f69dc222
James Shufelt <jshufelt@uark.edu>
2025-10-24 14:20:08 -0500

    bruh

--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -1,60 +1,53 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  -- opts for lualine
-  config = function ()
-    local status_ok, lualine = pcall(require('lualine').setup, {
-      options = {
-        icons_enabled = true,
-        theme = 'auto', 
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
-        disabled_filetypes = {
-          statusline = {},
-          --winbar = {},
-        },
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- opts for lualine
+	config = function()
+		local status_ok, lualine = pcall(require("lualine").setup, {
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					--winbar = {},
+				},
 
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-        }
-      },
-      sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
-        lualine_y = {},
-        lualine_z = {}
-      },
-      tabline = {},
-      winbar = {
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
 
-      },
-      inactive_winbar = {
+			extensions = {},
+		})
 
-      },
-
-      extensions = {}
-    })
-
-    if not status_ok then
-      vim.error("Error occured loading lualine")
-    end
-
-  end,
-
+		if not status_ok then
+			vim.error("Error occured loading lualine")
+		end
+	end,
 }
-
