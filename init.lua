ed8bd83c274f96ba7e4d0f349e24d18995614147
James Shufelt <jshufelt@uark.edu>
2025-11-09 19:17:43 -0600

    some small changes

--- init.lua
+++ init.lua
@@ -1,11 +1,10 @@
 --require("plugins.lua") require("themes.lua")
 --require("keybindings.lua"
 --print("Hello James Shufelt!")
-require "user.options"
-require "user.keyMap"
-require "user.pkmgr"
-require "user.plkeys"
-require "user.lspkeys"
+require("user.options")
+require("user.keyMap")
+require("user.pkmgr")
+require("user.plkeys")
+--require "user.lspkeys"
 --require "user.plugins.lsp.lspconfig"
 --vim.cmd [[colorscheme habamax]]
-
