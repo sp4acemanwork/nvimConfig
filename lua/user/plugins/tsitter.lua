d1a59fd19605bc7a706c228e1bb9e92eb509142e
James Shufelt <jshufelt@uark.edu>
2025-12-26 21:31:24 -0600

    changes

--- lua/user/plugins/tsitter.lua
+++ lua/user/plugins/tsitter.lua
@@ -1,18 +1,29 @@
 return {
-  --TODO improve the way this is loaded and introduce error handling oppon loading plugin
-  "nvim-treesitter/nvim-treesitter",
-  build = function()
-    require("nvim-treesitter.install").update({ with_sync = true })()
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	config = function()
+		-- Protected call to avoid crashing if the plugin is missing/corrupted
+		local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+		if not status_ok then
+			return
+		end
 
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			-- 1. Empty list: do not pre-install anything
+			ensure_installed = {},
 
-    configs.setup({
-      auto_install = true,
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+			-- 2. Auto-install parsers when you open a file that needs one
+			auto_install = true,
+
+			-- 3. Core highlighting enabled
+			sync_install = false,
+			ignore_install = {},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+		})
+	end,
 }
